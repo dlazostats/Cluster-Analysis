c9e57b0ee43ea4ba31df311310b8b6921f5b4d45
diego <alonso.lazo1540@gmail.com>
2022-02-10 10:06:53 -0500

    segundo script

---
+++ cluster_analysis_2.R
@@ -0,0 +1,9 @@
+## Cluster Analysis 2
+##-------------------
+library(mlr3cluster)
+library(mlr3)
+library(mlr3viz)
+
+### list of measures and learners
+mlr_measures$keys("clust")
+mlr_learners$keys("clust")
